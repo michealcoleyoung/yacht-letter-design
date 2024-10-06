831a3b8a5df567ff335da93b1e677cab00b3cdc2
michealcoleyoung <coleyoungmusic@gmail.com>
2024-10-06 01:29:59 -0500

    created updateText function to handle dynamic updates to text

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const textInput = document.getElementById('textInput');
-const textOutput = document.getElementById('outputText');
 
-textInput.addEventListener('input', function() {
+function updateText() {
+    const textInput = document.getElementById('textInput');
+    const textOutput = document.getElementById('outputText');
+
+    textInput.addEventListener('input', function() {
     textOutput.textContent = this.value;
 });
+}
